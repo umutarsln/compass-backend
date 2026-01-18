1f98192df686b7e7dc92920c52ad9cf9bcb8bdb0
DurmushKartci <durmusikartci@gmail.com>
2026-01-18 13:37:21 +0300

    port düzenlemesi

--- src/main.ts
+++ src/main.ts
@@ -48,6 +48,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
+  await app.listen(process.env.PORT ?? 7602, '0.0.0.0');
 }
 bootstrap();
